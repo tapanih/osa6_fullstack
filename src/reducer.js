fc378f58a5773904ed4c390452c508ccf8325845
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-21 13:57:14 +0300

    Lisätty vaaditut toiminnallisuudet

--- src/reducer.js
+++ src/reducer.js
@@ -8,12 +8,16 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
+      state = {...state, good: state.good + 1}
       return state
     case 'OK':
+      state = {...state, ok: state.ok + 1}
       return state
     case 'BAD':
+      state = {...state, bad: state.bad + 1}
       return state
     case 'ZERO':
+      state = initialState
       return state
     default: return state
   }
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -32,4 +32,50 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero changes all fields to 0', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const state = {
+      good: 4,
+      ok: 3,
+      bad: 5
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+  })
 })
