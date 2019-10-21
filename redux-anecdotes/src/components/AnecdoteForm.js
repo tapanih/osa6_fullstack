cc4a34cf35a2171c4d83bf73e68dd8524b5071e8
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-21 17:08:54 +0300

    Näytetään notifikaatio anekdootin lisäämisen jälkeen

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = (props) => {
   const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     props.store.dispatch(createAnecdote(content))
+    props.store.dispatch(showNotification(props.store.dispatch, `you added '${content}'`))
     event.target.anecdote.value = ''
   }
 
