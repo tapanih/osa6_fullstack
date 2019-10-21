f62b31e0fb53de7948290eb232cd8216bb4d945d
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-21 15:27:40 +0300

    Eriytetty anekdoottilistan näyttäminen omaksi komponentikseen

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { voteById } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    props.store.dispatch(voteById(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   )
